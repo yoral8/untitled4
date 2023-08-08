8e759cec1dcfb32dd3dfeb83e535f9b013ecd208
ZeynepBaglars <zeynepbaglars@gmail.com>
2023-08-09 00:05:06 +0300

    task -3

--- src/gun01/zeynep2.java
+++ src/gun01/zeynep2.java
@@ -3,5 +3,6 @@ package gun01;
 public class zeynep2 {
     public static void main(String[] args) {
         System.out.println("zeynepten selamlar");
+        System.out.println("merhaba");
     }
 }
